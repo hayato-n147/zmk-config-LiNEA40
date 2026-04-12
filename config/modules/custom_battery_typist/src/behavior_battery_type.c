79eb6dffd128ce0c4945d8fcff5b827195da6bda
hayato-n147 <nrht.2578.714@gmail.com>
2026-04-12 13:07:02 +0900

    Update behavior_battery_type.c

--- config/modules/custom_battery_typist/src/behavior_battery_type.c
+++ config/modules/custom_battery_typist/src/behavior_battery_type.c
@@ -61,12 +61,19 @@ struct char_keycode {
 
 static const struct char_keycode CHAR_MAP[] = {
     ['0'] = { .keycode = HID_KEY_0, .shift = false },
-    ['1'] = { .keycode = HID_KEY_1, .shift = false },
-    /* ... 2〜9も同様 ... */
+    ['1'] = { .keycode = HID_KEY_1, .shift = false },    
+    ['2'] = { .keycode = HID_KEY_2, .shift = false },    
+    ['3'] = { .keycode = HID_KEY_3, .shift = false },    
+    ['4'] = { .keycode = HID_KEY_4, .shift = false },    
+    ['5'] = { .keycode = HID_KEY_5, .shift = false },    
+    ['6'] = { .keycode = HID_KEY_6, .shift = false },    
+    ['7'] = { .keycode = HID_KEY_7, .shift = false },    
+    ['8'] = { .keycode = HID_KEY_8, .shift = false },    
+    ['9'] = { .keycode = HID_KEY_9, .shift = false },    
     ['L'] = { .keycode = HID_KEY_L, .shift = true },
     ['R'] = { .keycode = HID_KEY_R, .shift = true },
     [' '] = { .keycode = HID_KEY_SPACE, .shift = false },
-    [':'] = { .keycode = HID_KEY_'SEMICOLON, .shift = true },  /* US: Shift+; */
+    [':'] = { .keycode = HID_KEY_SEMICOLON, .shift = true },  /* US: Shift+; */
     ['='] = { .keycode = HID_KEY_MINUS, .shift = true },
     ['%'] = { .keycode = HID_KEY_5, .shift = true },           /* US: Shift+5 */
     ['-'] = { .keycode = HID_KEY_MINUS, .shift = false },
